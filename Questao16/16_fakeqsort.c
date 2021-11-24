4279d415ac7f3f501b75b6a1ddbdba4804401e7e
TheTheusz <94933473+TheTheusz@users.noreply.github.com>
2021-11-23 22:33:34 -0300

    Create 16_fakeqsort.c

---
+++ Questao16/16_fakeqsort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int comparar (const void *a, const void *b) { // função comparador que serve para comparar os termos do vetor valores na função qsort.
+  return ( *(int*)a - *(int*)b );  
+} 
+
+void fakeqsort(float *valor, int tamanho, int (*comparador)(const void *p1, const void *p2)){ // função qsort
+
+  for(int i = 0; i < tamanho; i++){ 
+    for(int j = i+1; j < tamanho; j++){
+      if(comparador(valor+i,valor+j)>0){ // realiza a função comparador com os endereços de memorias a ser comparado.
+        float aux;
+        aux = valor[j];
+        valor[j] = valor[i];
+        valor[i] = aux;
+      }
+    }
+  }
+}
+
+int main(void){
+  int n;
+  clock_t tempo[2];
+
+  tempo[0] = clock();
+  
+  printf("Quantos termos deseja digitar?\n"); 
+  scanf("%d",&n); // Recebe a quantidade de termos do vetor
+
+  float *valores;
+  valores = malloc(n * sizeof(float)); // Alocacao dinamica do vetor
+
+  for(int z = 0; z < n; z++){ // Realiza a leitura dos valores de cada termo do vetor
+    printf("digite o valor do %d termo:",z+1);
+    scanf("%f",&valores[z]);
+  }
+
+  fakeqsort(valores,n,comparar); // Reordena o vetor em ordem crescente
+
+
+  printf("\nOs numeros em ordem crescente é igual a: \n");//Imprime a ordem crescente do valores
+  for(int k = 0; k < n; k++){
+    printf("%.1f ",valores[k]);
+  }
+  free(valores);
+
+  tempo[1] = clock();
+  double total = (tempo[1] - tempo[0]) * 1000.0 / CLOCKS_PER_SEC;
+printf("\nO tempo total gasto do programa foi: %g ms.", total);
+getchar();
+  
+return 0;
+}
